9bd7ba1dd45ea0ccfa518cffda3bd2c1390b05a3
Erich Querner <erichquerner@posteo.org>
2024-10-14 19:27:02 +0200

    finish exercise 02-07

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,14 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0 || n == 1{
+        return 1;
+    }
+
+    let mut result = 1;
+    for x in (1..n + 1){
+        result *= x;
+    }
+    result
 }
 
 #[cfg(test)]
